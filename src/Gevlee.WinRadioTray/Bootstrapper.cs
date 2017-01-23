44409f2ae2bb777c64565326e1bde8f88e78db79
gevlee <gevleeog@gmail.com>
2017-01-23 22:40:33 +0100

    Initialize app

---
+++ src/Gevlee.WinRadioTray/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using Caliburn.Micro;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace Gevlee.WinRadioTray
+{
+	public class Bootstrapper : BootstrapperBase
+	{
+		private TaskbarIcon sysTrayIcon;
+
+		public Bootstrapper()
+		{
+			Initialize();
+		}
+
+		protected override void OnStartup(object sender, StartupEventArgs e)
+		{
+			base.OnStartup(sender, e);
+			App theApp = sender as App;
+			sysTrayIcon = (TaskbarIcon)theApp.FindResource("MainSystemTrayIcon");
+		}
+
+		protected override void OnExit(object sender, EventArgs e)
+		{
+			sysTrayIcon.Dispose();
+			base.OnExit(sender, e);
+		}
+	}
+}
